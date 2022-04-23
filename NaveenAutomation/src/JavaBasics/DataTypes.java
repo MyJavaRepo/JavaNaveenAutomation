9ff573289af2ca968a091ffa46c39b7cb022ef26
manoj.vyavahare <manoj.vyavahare@riomed.com>
2022-04-23 21:24:56 +0530

    Code Push for First commit

---
+++ NaveenAutomation/src/JavaBasics/DataTypes.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Data Types
+
+		int a=10; 
+		char c='A';
+		double d=11.25;
+		String s ="Manoj";		
+	}
+}
