f1a14f573ff0d4578856721e99d4e38e33c51f59
manoj.vyavahare <manoj.vyavahare@riomed.com>
2022-04-24 09:39:20 +0530

    Datatypes Concepts

--- NaveenAutomation/src/JavaBasics/DataTypes.java
+++ NaveenAutomation/src/JavaBasics/DataTypes.java
@@ -5,10 +5,13 @@ public class DataTypes {
 	// Manoj Vyavahare
 	public static void main(String[] args) {
 		// Data Types
-
-		int a=10; 
+		// int, double, char, boolean are the premitive data types
+		
+		int a=1000900004; 
 		char c='A';
 		double d=11.25;		
-		String s ="Manoj";		
+		boolean b=true;
+		String s ="Manoj";	 // String is not data type, it is a class.
+		System.out.println(a);
 	}
 }
